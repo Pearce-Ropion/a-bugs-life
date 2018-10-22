6e28f963177797041de907f60bbf58281446cba4
Vineet Joshi <vineet0713@gmail.com>
2018-10-22 15:55:00 -0700

    Add functionality to get the JSON data from POST requests.

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,7 @@
 import argparse
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+import sqlite3
 
 parser = argparse.ArgumentParser()
 parser.add_argument('PORT_NUMBER', help='This specifies the port number to run the server.', type=int)
@@ -25,9 +27,23 @@ class Serv(BaseHTTPRequestHandler):
 		# write the contents of the file onto the screen
 		self.wfile.write(bytes(file_to_open, 'utf-8'))
 
+	def do_POST(self):
+		try:
+			content_length = int(self.headers['Content-Length'])
+			post_body_bytes = self.rfile.read(content_length)
+			json_string = post_body_bytes.decode('utf8').replace("'", '"')
+			data = json.loads(json_string)
+
+			for key in data:
+				print(key + ": " + str(data[key]))
+
+			self.send_response(200)
+			# self.send_header('Content-type', 'text/html')
+		except:
+			self.send_response(400)
+
+		self.end_headers()
+
 # starts the HTTP Daemon
 http_daemon = HTTPServer(('localhost', args.PORT_NUMBER), Serv)
 http_daemon.serve_forever()
-
-# write a function that accepts a POST request
-# also write more functions for database interactions (add to table, update table, etc.)
