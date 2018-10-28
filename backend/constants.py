91e9f44dae78545403e236cb8c564c1c1bfcf88b
Vineet Joshi <vineet0713@gmail.com>
2018-10-28 16:14:26 -0700

    Add functionality for updating the database.

--- backend/constants.py
+++ backend/constants.py
@@ -3,13 +3,11 @@ database = 'bugs.db'
 
 # table names
 tickets_table = 'tickets'
-attachments_table = 'attachments'
 users_table = 'users'
 
 # table attributes
-attributes = {tickets_table: ['id', 'summary','description', 'assignee', 'reporter', 'components', 'priority', 'severity', 'labels', 'attachments', 'status', 'resolution', 'created', 'modified', 'closed'],
-			  attachments_table: ['id', 'ticketid', 'name'],
+attributes = {tickets_table: ['id', 'summary','description', 'assignee', 'reporter', 'component', 'priority', 'severity', 'labels', 'status', 'resolution', 'created', 'modified', 'closed'],
 			  users_table: ['id', 'username', 'password', 'role']}
 
 # list of tables
-table_list = [tickets_table, attachments_table, users_table]
+table_list = [tickets_table, users_table]
--- backend/server.py
+++ backend/server.py
@@ -10,12 +10,12 @@ import database_accessor as db
 # Server inherits methods from BaseHTTPRequestHandler class
 class Server(BaseHTTPRequestHandler):
 	# built into BaseHTTPRequestHandler, which runs when we receive a GET request
-	def do_GET(self):
-		# required because the html, scss, and js files are in the app directory
-		path = '/..' + self.path
-		
+	def do_GET(self):		
+		# initially, the js files were in the parent directory, but got moved to backend directory (this one!)
+		# self.path = '/..' + self.path
+
 		try:
-			file_to_open = open(path[1:]).read()
+			file_to_open = open(self.path[1:]).read()
 			self.send_response(200)
 		except:
 			file_to_open = 'File Not Found!'
@@ -30,6 +30,7 @@ class Server(BaseHTTPRequestHandler):
 	# built into BaseHTTPRequestHandler, which runs when we receive a POST request
 	def do_POST(self):
 		try:
+			# removes the '/app/' from the start of self.path
 			path = self.path[5:]
 			if len(path) == 0:
 				raise Exception
@@ -41,21 +42,32 @@ class Server(BaseHTTPRequestHandler):
 
 			response_data = None
 
-			if path == 'test1':
-				db.insert_into_db(data, c.tickets_table)
-			elif path == 'test2':
-				response_data = db.get_table_data(c.tickets_table)
-			elif path == 'test3':
-				filter1 = ('id', 2)
-				print json.dumps(db.get_table_data(c.tickets_table, filter1), indent=4)
+			# if path == 'test1':
+			# 	db.insert_into_db(data, c.tickets_table)
+			# elif path == 'test2':
+			# 	response_data = db.get_table_data(c.tickets_table)
+			# elif path == 'test3':
+			# 	filter1 = ('id', 2)
+			# 	print json.dumps(db.get_table_data(c.tickets_table, filter1), indent=4)
 
-				filter2 = (
-					('labels', ["test1","test2"]),
-					('assignee', 'a1'),
-					('severity', 10),
-					('attachments', None)
-				)
-				print json.dumps(db.get_table_data(c.tickets_table, filter2), indent=4)
+			# 	filter2 = (
+			# 		('labels', ["test1","test2"]),
+			# 		('assignee', 'a1'),
+			# 		('severity', 10),
+			# 		('attachments', None)
+			# 	)
+			# 	print json.dumps(db.get_table_data(c.tickets_table, filter2), indent=4)
+			# else:
+			# 	raise Exception
+
+			if path == 'create':
+				# removes the 'id' key because it will be null
+				data.pop('id', None)
+				db.insert_into_db(data, c.tickets_table)
+			elif path == 'update':
+				db.update_db(data, c.tickets_table)
+			elif path == 'tickets':
+				response_data = db.get_table_data(c.tickets_table)
 			else:
 				raise Exception
 
@@ -66,6 +78,7 @@ class Server(BaseHTTPRequestHandler):
 				response_data_string = json.dumps(response_data)
 				response_data_bytes = response_data_string.encode()
 				self.wfile.write(response_data_bytes)
-		except:
+		except Exception as e:
 			self.send_response(400)
 			self.end_headers()
+			print 'Exception: ' + e
