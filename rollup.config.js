8ac66b9bac54f037665b6a2376a00438a2f47013
Pearce <pearce.ropion@me.com>
2018-10-29 10:37:48 -0700

    Build updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import builtins from 'rollup-plugin-node-builtins';
+import json from 'rollup-plugin-json';
 
 import { description, version } from './package.json';
 
@@ -8,7 +12,7 @@ export default {
     output: {
         dir: 'dist',
         file: 'bundle.js',
-        format: 'cjs',
+        format: 'es',
         banner: '/*'.concat(description, ' v', version, ' */'),
         globals: {
             react: 'React',
@@ -20,11 +24,20 @@ export default {
     plugins: [
         babel({
             exclude: ['node_modules/**', 'src/styles/*'],
-            plugins: ['@babel/external-helpers'],
         }),
         postcss({
             modules: true,
         }),
+        resolve({
+            preferBuiltins: false,
+        }),
+        builtins(),
+        commonjs({
+            namesExports: {
+                'node_modules/buffer/index.js': ['Buffer'],
+            }
+        }),
+        json(),
     ],
     external: ['react', 'react-dom', 'prop-types', 'semantic-ui-react'],
 };
